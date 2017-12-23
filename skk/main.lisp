ca6812d4fe14107eb116458e27f72d24cd7087ae
TANIGUCHI Masaya <mew@cat.ovh>
2017-12-23 22:01:16 +0900

    add skk-dictionary

---
+++ skk/main.lisp
@@ -0,0 +1,16 @@
+(in-package :cl)
+(defpackage :lime/skk/main
+  (:nicknames :lime/skk)
+  (:use :cl
+        :lime/core/dictionary
+        :lime/skk/text
+        :lime/skk/pattern
+        :lime/skk/lisp)
+  (:export skk-dictionar
+           skk-text-dictionary
+           skk-pattern-dictionary
+           skk-lisp-dictionary))
+(in-package :lime/skk/main)
+
+(defclass skk-dictionary (skk-text-dictionary skk-pattern-dictionary skk-lisp-dictionary) ())
+(defmethod lookup ((d skk-dictionary) (s string)))
