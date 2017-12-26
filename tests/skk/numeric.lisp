c783cc1e5e18fc21bca11cef8f3c47ca26adf7ff
asciian <asciian@users.noreply.github.com>
2017-12-26 22:30:31 +0900

    rename tests

--- tests/skk/numeric.lisp
+++ tests/skk/numeric.lisp
@@ -8,6 +8,6 @@
 (defparameter *dictionary* 
   (make-instance 'skk-numeric-dictionary :pathname #p"./SKK-JISYO.L"))
 
-(test skk-numeric-dictionary
+(test test-skk-numeric-dictionary
       ;;辞書の検索
       (is (string= "12月24日" (first (convert *dictionary* "12/24")))))
