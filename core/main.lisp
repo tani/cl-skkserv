8ac8bb472208e15a203c485727d019f352ac828e
asciian <asciian@users.noreply.github.com>
2017-12-26 13:27:49 +0900

    add process layer

--- core/main.lisp
+++ core/main.lisp
@@ -3,11 +3,13 @@
   (:nicknames :lime/core)
   (:use :cl)
   (:import-from :lime/core/dictionary dictionary convert complete)
-  (:import-from :lime/core/handler handler)
+  (:import-from :lime/core/handler handle)
   (:import-from :lime/core/mixed mixed-dictionary)
+  (:import-from :lime/core/process process)
   (:export dictionary
            convert
            complete
            mixed-dictionary
-           handler))
+           handle
+           process))
 (in-package :lime/core/main)
