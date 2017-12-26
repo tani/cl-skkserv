3f3b38ed513d62ced216693a433bdf64e136921b
asciian <asciian@users.noreply.github.com>
2017-12-26 22:30:56 +0900

    add test for process

---
+++ tests/core/handler.lisp
@@ -0,0 +1,18 @@
+(in-package :cl-user)
+(defpackage :lime/tests/core/handler
+  (:use :cl :1am 
+        :lime/skk/main
+        :lime/core/dictionary
+        :lime/core/handler))
+(in-package :lime/tests/core/handler)
+
+(defparameter *dictionary*
+  (make-instance 'skk-dictionary :pathname #p"./SKK-JISYO.L"))
+
+(test test-handler
+      (is (equalp (multiple-value-list (handle "1a " *dictionary*))
+                  (list 1 (format nil "1/~{~A/~}~%" (convert *dictionary* "a")))))
+      (is (equalp (multiple-value-list (handle "4a " *dictionary*))
+                  (list 4 (format nil "1/~{~A/~}~%" (complete *dictionary* "a"))))))
+
+          
