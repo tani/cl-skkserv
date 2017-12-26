84131dea96fbb90a04717f69b4e47843a31ed16a
asciian <asciian@users.noreply.github.com>
2017-12-26 22:53:48 +0900

    move mixed-dictionary

--- mixed/main.lisp
+++ mixed/main.lisp
@@ -1,9 +1,10 @@
 (in-package :cl-user)
-(defpackage :lime/core/mixed
+(defpackage :lime/mixed/main
+  (:nicknames :lime/mixed)
   (:use :cl)
   (:import-from :lime/core/dictionary dictionary convert complete)
   (:export mixed-dictionary))
-(in-package :lime/core/mixed)
+(in-package :lime/mixed/main)
 
 (defclass mixed-dictionary (dictionary)
   ((dictionaries :initarg :dictionaries :reader dictionaries)))
